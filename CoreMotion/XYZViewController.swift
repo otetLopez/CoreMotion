b49ce3a21c065b7014091414c107cc70beedaa13
otetLopez <otet.tud@gmail.com>
2020-01-21 17:21:57 -0500

    Move a square

--- CoreMotion/XYZViewController.swift
+++ CoreMotion/XYZViewController.swift
@@ -15,7 +15,7 @@ class XYZViewController: UIViewController {
     @IBOutlet weak var xlbl: UILabel!
     @IBOutlet weak var ylbl: UILabel!
     @IBOutlet weak var zlbl: UILabel!
-    
+    @IBOutlet weak var square: UIView!
     var motionManager: CMMotionManager!
     
     override func viewDidLoad() {
@@ -38,11 +38,29 @@ class XYZViewController: UIViewController {
             self.xlbl.text = "X: \(x)"
             self.ylbl.text = "Y: \(y)"
             self.zlbl.text = "Z: \(z)"
+            
+            self.moveSquare(x: CGFloat(accelerometerData.acceleration.x), y: CGFloat(accelerometerData.acceleration.y))
         }
         // Do any additional setup after loading the view.
     }
     
-
+    func moveSquare(x: CGFloat, y:CGFloat) {
+        let xPosition = square.frame.origin.x
+        let yPosition = square.frame.origin.y
+        
+        let width = square.frame.size.width
+        let height = square.frame.size.height
+        
+        // Get the device frame size
+        let screen = UIScreen.main.bounds
+        let screenWidth = screen.width
+        let screenHeight = screen.height
+        
+        UIView.animate(withDuration: 0) {
+            guard (xPosition + x >= 0 && xPosition + width + x <= screenWidth) && (yPosition - y >= 0 && yPosition + height - y <= screenHeight) else { return }
+            self.square.frame = CGRect(x: xPosition + x, y: yPosition - y, width: width, height: height)
+        }
+    }
     /*
     // MARK: - Navigation
 
