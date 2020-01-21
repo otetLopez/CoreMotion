0d6d89a3251695400c5dc6b7101a9687c722e0bf
otetLopez <otet.tud@gmail.com>
2020-01-21 16:29:38 -0500

    Image Tilt

--- CoreMotion/ViewController.swift
+++ CoreMotion/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpWallpaper()
+    }
+    
+    func setUpWallpaper() {
+        let min = CGFloat(-50)
+        let max = CGFloat(50)
+        
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+        
+        xMotion.minimumRelativeValue = min
+        xMotion.maximumRelativeValue = max
+        yMotion.minimumRelativeValue = min
+        yMotion.maximumRelativeValue = max
+        
+        let motionEffectGroup = UIMotionEffectGroup()
+        motionEffectGroup.motionEffects = [xMotion, yMotion]
+        imageView.addMotionEffect(motionEffectGroup)
+        
     }
 
 
